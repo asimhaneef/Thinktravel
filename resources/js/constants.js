4f9b4f44941e768146add81e3ecc4371c5c94caa
Shahroz <49663783+Shahroz09@users.noreply.github.com>
2025-06-20 19:45:59 +0500

    app url fix

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -1,4 +1,4 @@
 export const APP_NAME = 'GSS Charity';
 export const APP_VERSION = '1.0.0';
 //export const APP_URL = 'http://localhost:8000/';
-export const APP_URL = 'http://School-loadbalancer-1361451310.us-east-2.elb.amazonaws.com/public/';
+export const APP_URL = 'https://School-loadbalancer-1361451310.us-east-2.elb.amazonaws.com/public/';
