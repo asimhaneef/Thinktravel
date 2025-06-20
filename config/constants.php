871b9d085ff3205feb254c1938328e01b929ee0c
Shahroz <49663783+Shahroz09@users.noreply.github.com>
2025-06-20 22:31:14 +0500

    constants file issue

--- config/constants.php
+++ config/constants.php
@@ -7,7 +7,7 @@ return [
     // 'IMAGES_PATH' =>'public/images', 
 
     //for local
-    'ASSETS_PATH' =>'assets', 
-    'IMAGES_PATH' =>'images', 
+    'ASSETS_PATH' =>'https://thinktravel.nexium.global/assets', 
+    'IMAGES_PATH' =>'https://thinktravel.nexium.global/images', 
     
 ];
